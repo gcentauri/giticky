90a240c454b15564e9bf240c30e0a2b40b4b7ee6
Grant Shoshin Shangreaux <gcentauri@lincolnix.net>
2016-09-25 00:35:22 -0500

    finally have some sort of working multi project setup using flatpages

--- giticky.py
+++ giticky.py
@@ -7,15 +7,19 @@ import re
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = '.md'
-FLATPAGES_ROOT = '/home/grant/projects/gitick-projects/giticky.gitick'
+
+app = Flask(__name__)
 
 IS_MD_FILE = re.compile('\.md$')
 
-app = Flask(__name__)
-app.config.from_object(__name__)
-TICKETS = FlatPages(app) #this is a flatpages.pages object, a collection of page(s)
+PROJECTS = ['wwb', 'giticky']
+
 TAG_DICT = defaultdict(list)
+
+def init_tickets_for(proj):
+    config = proj + '.cfg'
+    app.config.from_pyfile(config)
+    return FlatPages(app, name=proj)
 
 ## tag_list is used in the case that page['tags'] is a string
 ## it removes excess whitespace and makes the tag lowercase to avoid duplicates
@@ -29,10 +33,13 @@ def tag_list(tags):
         return [t.strip().lower() for t in tags]
     else: return []
     
-    
-TICKET_PATHS = [ticket.path for ticket in TICKETS] #list of strings
-TICKET_PATHS.sort()
-TOPDIRS = [d for d in listdir(FLATPAGES_ROOT) if d[0] != '.'] 
+def get_ticket_paths(tickets):
+    ticket_paths = [ticket.path for ticket in tickets] #list of strings
+    ticket_paths.sort()
+    return ticket_paths
+
+def get_subdirs(proj):
+    return [d for d in listdir(proj) if d[0] != '.'] 
 
 ## lod is a list of directories (strings) tickets is a flatpages.pages object
 ## returns a dict of directory : list of tickets (tickets are page objects)
@@ -42,39 +49,24 @@ def project_index(lod, tickets):
         [index_dict[d].append(t) for t in tickets if t.path[:len(d)] == d]
     return index_dict
 
-INDEX_DICT = project_index(TOPDIRS, TICKETS)
 
-## like the above function except just uses paths instead of tickets
-## using paths seems to be fine, because in the templates you can use the
-## url_for('ticket', path=ticket_path) to generate links to the pages themselves
-def project_index_paths(lod, t_paths):
-    index_dict = defaultdict(list)
-    for d in lod:
-        [index_dict[d].append(t) for t in t_paths if t[:len(d)] == d]
-    return index_dict
+# def fill_tags(ticket):
+#     for tag in tag_list(ticket['tags']):
+#         TAG_DICT[tag].append(ticket.path)
 
-PATH_DICT = project_index_paths(TOPDIRS, TICKET_PATHS)
+# def init_dicts():
+#     for ticket in TICKETS:
+#         fill_tags(ticket)
 
-def fill_tags(ticket):
-    for tag in tag_list(ticket['tags']):
-        TAG_DICT[tag].append(ticket.path)
-
-def init_dicts():
-    for ticket in TICKETS:
-        fill_tags(ticket)
-
-        
 @app.route('/')
-def index():
-    tags = list(TAG_DICT.keys())
-    tags.sort()
-    subdirs = list(INDEX_DICT.keys())
-    data = {
-        'tickets': INDEX_DICT,
-        'tags': tags,
-        'subdirs' : subdirs,
-    }
-    return render_template('index.html', data=data)  
+def top():
+    projects = PROJECTS
+    return render_template('top.html', projects=projects)
+
+@app.route('/<project>')
+def index(project):
+    tickets = init_tickets_for(project)
+    return render_template('index.html', tickets=tickets)  
 
 @app.route('/<path:path>/')
 def ticket(path):
@@ -101,7 +93,8 @@ def tagged(tag):
     
     return render_template('tagged.html', tagged=tagged)
 
-# @app.route('/user/<user>/')
+
+# @GITICKY.route('/user/<user>/')
 # def user(user):
 #     paths = USER_DICT[user]
 #     paths.sort()
@@ -110,6 +103,5 @@ def tagged(tag):
 
 
 if __name__ == '__main__':
-    init_dicts()
     app.run(port=8000)
     
