5fb1d83dc0fe5a8ef35c6390dce09a4fff3a54a0
Grant Shoshin Shangreaux <gcentauri@lincolnix.net>
2016-09-28 15:14:29 -0500

    basic class definition and add_ticket functionality working

--- gitick.py
+++ gitick.py
@@ -1,63 +1,127 @@
 import sh
+from pathlib import Path
+from itertools import takewhile
+import operator
+import yaml
 
-def new():
-    gitick_project = 'gitick.' + input( 'project name: ' )
-    sh.mkdir( gitick_project )
-    sh.cd( gitick_project )
-    sh.echo( '.', _out='.gitick' )
-    sh.mkdir( 'new' )
-    sh.echo( '..', _out='new/.gitick' )
-    sh.mkdir( 'new/need-info' )
-    sh.echo( '../..', _out='new/need-info/.gitick' )
-    sh.git( 'init', '.' )
-    sh.git( 'add', '.' )
-    sh.git( 'commit', '-m', str("initial commit for " + gitick_project) )
-
-def home():
-    with open( '.gitick', 'r' ) as f:
-        l = f.read()
-        f.close()
-        return l.rstrip()
         
-def add():
-    curdir = str(sh.pwd()).rstrip()
-    sh.cd( home() )
-    title = input( 'New ticket name: ' )
-    ticket_file = ( 'new/' + title.replace(' ','-') + '.md' )
-    priority = str( 'priority: ' + input('Priority: ') )
-    tags = str('tags: ' + input('Tags (separate by commas): '))
-    
-    ## please show me a better way to do this, i'm just doing a dumb
-    ## translation of the sh script
-    
-    with open( ticket_file, 'w' ) as ticket:
-        ticket.write(priority + '\n')
-        ticket.write(tags + '\n')
-        ticket.write('date: ' + str(sh.date()))
-        ticket.write('reported: ' + str(sh.git('config', 'user.name')))
-        ticket.write('\n')
-        ticket.write(str('# ' + title))
-        ticket.write('\n')
-        ticket.write(str('# LOG'))
-        ticket.close()
+class Ticket:
+    ''' 
+    A ticket is a markdown file with a YAML header.  We would like to parse
+    this file and gather data like: title, meta, body, tags, mtime, etc etc, 
+    '''
+    def __init__(self, path):
+        self.path = Path(path)
+        self.modified = self.path.stat().st_mtime
+        self.name = self.path.name
 
-    sh.cd(curdir)
+    def meta(self):
+        return 
+    
         
-def user():
-    curdir = str(sh.pwd()).rstrip()
-    sh.cd( home() )
-
-    name = input( 'New user name: ' )
-    sh.mkdir( name )
-    sh.cd(name)
-    sh.mkdir('in-progress', 'need-info', 'test')
-    sh.echo( '../..', _out='in-progress/.gitick' )
-    sh.echo( '../..', _out='need-info/.gitick' )
-    sh.echo( '../..', _out='test/.gitick' )
-
-    sh.git('add', '.')
-    sh.git('commit', '-m', str("adding user " + name))
-
-    sh.cd(curdir)
+class Gitick:
+    '''
+    A Gitick object represents a git repository full of markdown
+    files that are Tickets.  
+    '''
+    tickets = []
     
-    
+    def __init__(self, path):
+        self.root = Path(path)
+        self.tickets = sorted(self.root.glob('**/*.md'))
+        
+    def __len__(self):
+        return len(self.tickets)
+
+    def __getitem__(self, position):
+        return self.tickets[position]
+
+    def add_ticket(self, title, priority, tags):
+        '''
+        title is a string
+        priority is an int from 1 to 9
+        tags is a list of strings
+        '''
+        curdir = str(sh.pwd()).rstrip()
+        sh.cd(self.root.as_posix())
+        ticket_file = ( 'new/' + title.replace(' ','-') + '.md' )
+        title = 'title: ' + title
+        priority = 'priority: ' + str(priority)
+        tags = 'tags: ' + '[' + ', '.join(tags) + ']'
+        date = 'date: ' + str(sh.date()).strip()
+        reported = 'reported: ' + str(sh.git('config', 'user.name'))
+        
+        with open( ticket_file, 'w' ) as ticket:
+
+            metaString = '\n'.join([title, priority, tags, date, reported])
+            headings = '\n'.join(['## summary', '# LOG'])
+            
+            ticket.write(metaString)
+            ticket.write('\n')
+            ticket.write(headings)
+            ticket.close()
+
+        self.tickets = sorted(self.tickets + [Path(self.root, ticket_file)])
+        sh.cd(curdir)
+
+    # allows tof get in-progress, testing, need-info (must have tix directly
+    # under parent tho)
+    def get_tickets_from_parent( self, parent ):
+        return [t for t in self.tickets if t.parent.name == parent]
+
+    def in_progress( self ):
+        return self.get_tickets_from_parent("in-progress")
+
+    def new_tickets( self):
+        return self.get_tickets_from_parent("new")
+
+##---------------------------------------------------------##
+## just for testing ##
+
+PROJ = Gitick('/home/grant/projects/gitick-projects/giticky.gitick')
+TOPDIR = [x for x in PROJ.root.iterdir() if x.is_dir() and x.name[0] is not '.']
+TIX = PROJ.tickets
+NEW = PROJ.new_tickets()
+WIP = PROJ.in_progress()
+
+
+## old gitick functions
+
+# def home(self):
+#     with open( '.gitick', 'r' ) as f:
+#         l = f.read()
+#         f.close()
+#     return l.rstrip()
+
+# def new():
+#     gitick_project = 'gitick.' + input( 'project name: ' )
+
+#     sh.mkdir( gitick_project )
+#     sh.cd( gitick_project )
+#     sh.echo( '.', _out='.gitick' )
+#     sh.mkdir( 'new' )
+#     sh.echo( '..', _out='new/.gitick' )
+#     sh.mkdir( 'new/need-info' )
+#     sh.echo( '../..', _out='new/need-info/.gitick' )
+#     sh.git( 'init', '.' )
+#     sh.git( 'add', '.' )
+#     sh.git( 'commit', '-m', str("initial commit for " + gitick_project) )
+
+        
+        
+# def user():
+#     curdir = str(sh.pwd()).rstrip()
+#     sh.cd( home() )
+
+#     name = input( 'New user name: ' )
+#     sh.mkdir( name )
+#     sh.cd(name)
+#     sh.mkdir('in-progress', 'need-info', 'test')
+#     sh.echo( '../..', _out='in-progress/.gitick' )
+#     sh.echo( '../..', _out='need-info/.gitick' )
+#     sh.echo( '../..', _out='test/.gitick' )
+
+#     sh.git('add', '.')
+#     sh.git('commit', '-m', str("adding user " + name))
+
+#     sh.cd(curdir)
